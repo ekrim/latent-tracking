fixed normalization

data.py
@@ -45,7 +45,7 @@ def load_joints(f):
 
 
 class MRSADataset(Dataset):
-  def __init__(self, subjects=None, gestures=None, image=False, max_buffer=20, size=64):
+  def __init__(self, subjects=None, gestures=None, image=False, max_buffer=4, size=64):
     subjects = SUBJECTS if subjects is None else subjects
     gestures = GESTURES if gestures is None else gestures
 
@@ -58,8 +58,11 @@ class MRSADataset(Dataset):
     return len(self.depth_files)
     
   def __getitem__(self, idx): 
-
-    jts_tens = torch.from_numpy(self.joints[idx])
+  
+    jts = self.joints[idx].reshape((-1,3))
+    jts -= np.mean(jts, axis=0)[None,:]
+    jts = jts/80
+    jts_tens = torch.from_numpy(jts.flatten())
 
     if self.image:
       img = load_image_bin(self.depth_files[idx])
@@ -70,7 +73,7 @@ class MRSADataset(Dataset):
       delta_width = max_dim - width 
       delta_height = max_dim - height
       
-      left = np.random.randint(0, delta_width+1)
+      left = np.random.randint(0, np.ceil(delta_width+1/2))
       top = np.random.randint(0, delta_height+1)
 
       padded = F.pad(pil_img, (left, top, delta_width-left, delta_height-top))

eval.py
@@ -13,7 +13,7 @@ from torch.utils.data import Dataset, DataLoader
 
 import utils
 import geometry as geo
-from data import MRSADataset
+from data import MRSADataset, SUBJECTS, GESTURES
 from model import RealNVP, PoseModel
 
 
@@ -45,21 +45,26 @@ if __name__ == '__main__':
 
   
   if args.display_flow:
+    n_plots = 9
     # display generated poses
-    hand_pose = gen_fnc(np.random.randn(9, dim_in))
+    hand_pose = gen_fnc(np.random.randn(n_plots, dim_in))
     
     fig = plt.figure()
-    for i in range(9):
-      ax = fig.add_subplot('33{:d}'.format(i+1), projection='3d')
-      geo.plot_skeleton(hand_pose[i], ax, col='b')
+    for i in range(n_plots):
+      ax = fig.add_subplot('33{:d}'.format(i+1))
+      geo.plot_skeleton2d(hand_pose[i], ax, autoscale=False)
 
 
   if args.display_pose:
-    idx = 0
-    subject = 'P0'
-    gesture = 'T'
+    idx = np.random.randint(0, 500)
+    print(idx)
+    subject = np.random.choice(SUBJECTS)
+    print(subject)
+    gesture = np.random.choice(GESTURES)
+    print(gesture)
+
     # regress poses and display
-    ds = MRSADataset(subjects=[subject], gestures=[gesture], max_buffer=8, image=True)
+    ds = MRSADataset(subjects=[subject], gestures=[gesture], max_buffer=4, image=True)
     dl = DataLoader(
       ds,
       num_workers=2,
@@ -67,13 +72,24 @@ if __name__ == '__main__':
       shuffle=False)
 
     batch = dl.__iter__().__next__()
-    pred_pose = pose_fnc(batch['img'])[idx]
-    true_pose = batch['jts'].detach().cpu().numpy()[idx]
-    img = batch['img'].detach().cpu().numpy()[idx,0]
+    pred_pose = pose_fnc(batch['img'])
+    true_pose = batch['jts'].detach().cpu().numpy()
+    img = batch['img'].detach().cpu().numpy()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    geo.joints_over_depth(true_pose, img, ax)
+    for i in range(3):
+
+      fig = plt.figure()
+      ax = fig.add_subplot(131)
+      ax = geo.plot_skeleton2d(pred_pose[idx+1], ax, autoscale=False)
+      ax.set_xlim([-0.8, 0.8])
+      ax.set_ylim([-0.8, 0.8])
+      ax = fig.add_subplot(132)
+      ax = geo.plot_skeleton2d(true_pose[idx+1], ax, autoscale=False)
+      ax.set_xlim([-0.8, 0.8])
+      ax.set_ylim([-0.8, 0.8])
+      ax = fig.add_subplot(133)
+      ax.imshow(np.clip(img[idx,0], 0.9, 1), cmap='Greys_r')
+      #geo.joints_over_depth(true_pose, img, ax)
 
 
   if args.interpolate:   

geometry.py
@@ -100,7 +100,7 @@ def plot_skeleton(x, ax):
   return ax
 
 
-def plot_skeleton2d(x, ax):
+def plot_skeleton2d(x, ax, autoscale=True):
   if x.shape[-1] != 3:
     x = x.reshape((-1, 3))
 
@@ -113,20 +113,21 @@ def plot_skeleton2d(x, ax):
   ax.set_ylabel('y')
   ax.set_aspect('equal')
 
-  ranges = np.concatenate([ 
-    np.min(x, axis=0)[None,:],
-    np.max(x, axis=0)[None,:]], axis=0)
+  if autoscale:
+    ranges = np.concatenate([ 
+      np.min(x, axis=0)[None,:],
+      np.max(x, axis=0)[None,:]], axis=0)
 
-  max_range = np.ceil(np.max(ranges[1] - ranges[0]))
-  mean_range = np.mean(ranges, axis=0)
- 
-  new_range = np.concatenate([
-    (mean_range-max_range/2)[None,:],
-    (mean_range+max_range/2)[None,:],
-  ])
-  
-  ax.set_xlim(new_range[:,0])  
-  ax.set_ylim(new_range[:,1])  
+    max_range = np.ceil(np.max(ranges[1] - ranges[0]))
+    mean_range = np.mean(ranges, axis=0)
+   
+    new_range = np.concatenate([
+      (mean_range-max_range/2)[None,:],
+      (mean_range+max_range/2)[None,:],
+    ])
+    
+    ax.set_xlim(new_range[:,0])  
+    ax.set_ylim(new_range[:,1])  
   return ax
 
 
@@ -135,21 +136,36 @@ def joints_over_depth(jts, img, ax):
   ax.imshow(img[::-1,:], cmap='Greys_r')
 
   n_rows, n_cols = img.shape
+  
+  bounds = find_bounds(img)
 
   new_jts = jts.copy().reshape((-1,3))
-  new_jts = normalize_dim(new_jts, n_cols, 0)
-  new_jts = normalize_dim(new_jts, n_rows, 1)
+
+  new_jts = normalize_dim(new_jts, bounds[3], bounds[1], 1)
+  x_min = np.min(new_jts[:,0])
+  new_jts[:,0] += bounds[0] - x_min
   
-  plot_skeleton2d(new_jts, ax)
+  plot_skeleton2d(new_jts, ax, autoscale=False)
   return ax
 
 
-def normalize_dim(x, goal_max, dim):
+def find_bounds(img, thresh=0.7):
+  truth = img > thresh  
+  col_bounds = np.arange(img.shape[1])[np.any(truth, axis=0)][[0,-1]]
+  row_bounds = np.arange(img.shape[0])[np.any(truth, axis=1)][[0,-1]]
+  return col_bounds[0], img.shape[0]-row_bounds[0], col_bounds[1], img.shape[0]-row_bounds[1]
+
+
+def normalize_dim(x, goal_min, goal_max, dim):
+  # put in [0,1]
   min_val = np.min(x[:,dim])
   x -= min_val
   max_val = np.max(x[:,dim])
   x = x/max_val
-  x *= goal_max
+
+  mag = goal_max - goal_min
+  x *= mag
+  x[:,dim] += goal_min
   
   return x
   